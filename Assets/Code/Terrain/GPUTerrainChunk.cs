c69bafff8b3cd0ce35c2de771ba01ba81f50efc4
Rory Dungan <rorydungan@gmail.com>
2018-04-29 17:54:19 +1000

    Start working on new terrain shader

---
+++ Assets/Code/Terrain/GPUTerrainChunk.cs
@@ -0,0 +1,132 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+[RequireComponent(typeof(MeshFilter)), RequireComponent(typeof(MeshRenderer))]
+public class GPUTerrainChunk : MonoBehaviour
+{
+    /// <summary>
+    /// Size of terrrain chunk meshes.
+    /// </summary>
+    private int Size = 256;
+
+    [SerializeField]
+    private Vector3 scale = Vector3.one;
+
+    private MeshFilter meshFilter;
+
+    private MeshFilter MeshFilter
+    {
+        get
+        {
+            if (meshFilter == null)
+            {
+                meshFilter = GetComponent<MeshFilter>();
+                Assert.IsNotNull(meshFilter);
+            }
+            return meshFilter;
+        }
+    }
+
+    public Material Material
+    {
+        get
+        {
+            var renderer = GetComponent<MeshRenderer>();
+            Assert.IsNotNull(renderer, "No MeshRenderer assigned to TerrainGenerator object");
+
+            return renderer.material;
+        }
+        set
+        {
+            var renderer = GetComponent<MeshRenderer>();
+            Assert.IsNotNull(renderer, "No MeshRenderer assigned to TerrainGenerator object");
+
+            renderer.material = value;
+        }
+    }
+
+    private static Vector3[] GenerateVerts(
+        int size,
+        Vector3 scale
+    )
+    {
+        var v = new Vector3[size * size];
+        for (var i = 0; i < size; i++)
+        {
+            for (var j = 0; j < size; j++)
+            {
+                v[i * size + j].x =
+                    i * scale.x / (size - 1);
+
+                v[i * size + j].y = 0f;
+
+                v[i * size + j].z =
+                    j * scale.z / (size - 1);
+            }
+        }
+        return v;
+    }
+
+    private static int[] GenerateTris(
+        int size
+    )
+    {
+        var total = size - 1;
+
+        var tris = new int[total * total * 6];
+        for (var i = 0; i < total; i++)
+        {
+            for (var j = 0; j < total; j++)
+            {
+                var idx = i * total * 6 + j * 6;
+                tris[idx] = i * size + j;
+
+                tris[idx + 1] = i * size + j + 1;
+                tris[idx + 2] = i * size + j + size;
+                tris[idx + 3] = i * size +j + 1;
+                tris[idx + 4] = i * size + j + size + 1;
+                tris[idx + 5] = i * size + j + size;
+            }
+        }
+
+        return tris;
+    }
+
+    private static Vector2[] GenerateUVs(int size)
+    {
+        var uvs = new Vector2[size * size];
+        for (var i = 0; i < size; i++)
+        {
+            for (var j = 0; j < size; j++)
+            {
+                uvs[i * size + j].x = (float)i / size;
+                uvs[i * size + j].y = (float)j / size;
+            }
+        }
+
+        return uvs;
+    }
+
+    [ContextMenu("Update mesh")]
+    public void UpdateMesh()
+    {
+        var sw = Stopwatch.StartNew();
+
+        var mesh = MeshFilter.mesh = new Mesh
+        {
+            name = "TerrainChunk",
+            vertices = GenerateVerts(Size, scale),
+            triangles = GenerateTris(Size),
+            uv = GenerateUVs(Size)
+        };
+
+        mesh.RecalculateNormals();
+
+        sw.Stop();
+        var elapsedMs = (double)sw.ElapsedTicks / Stopwatch.Frequency * 1000D;
+        UnityEngine.Debug.Log("UpdateMesh: " + elapsedMs + "ms");
+    }
+}
---
+++ Assets/Shaders/Terrain.shader
@@ -0,0 +1,72 @@
+﻿Shader "Custom/Terrain"
+{
+    Properties
+    {
+        [NoScaleOffset] _MainTex ("Texture", 2D) = "white" {}
+    }
+    SubShader
+    {
+        // indicate that our pass is the "base" pass in forward
+        // rendering pipeline. It gets ambient and main directional
+        // light data set up; light direction in _WorldSpaceLightPos0
+        // and color in _LightColor0
+        Tags
+        {
+            "LightMode" = "ForwardBase"
+        }
+
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+            // make fog work
+            //#pragma multi_compile_fog
+
+            #include "UnityCG.cginc" // for UnityObjectToWorldNormal
+            #include "UnityLightingCommon.cginc" // for _LightColor0
+
+            struct v2f
+            {
+                float2 uv : TEXCOORD0;
+                //UNITY_FOG_COORDS(1)
+                float4 vertex : SV_POSITION;
+                float4 diff : COLOR0; // diffuse lighting color
+            };
+
+            sampler2D _MainTex;
+            float4 _MainTex_ST;
+
+            v2f vert (appdata_base v)
+            {
+                v2f o;
+                o.vertex = UnityObjectToClipPos(v.vertex);
+                o.uv = v.texcoord;
+                // get the vertex normal in world space
+                half3 worldNormal = UnityObjectToWorldNormal(v.normal);
+                // dot product between the normal and light direction for
+                // standard diffuse (Lambert) lighting
+                half nl = max(0, dot(worldNormal, _WorldSpaceLightPos0.xyz));
+                // factor in the light color
+                o.diff = nl * _LightColor0;
+
+                //UNITY_TRANSFER_FOG(o,o.vertex);
+                return o;
+            }
+
+            fixed4 frag (v2f i) : SV_Target
+            {
+                // sample the texture
+                fixed4 col = tex2D(_MainTex, i.uv);
+
+                // multiply by lighting
+                col *= i.diff;
+
+                // apply fog
+                //UNITY_APPLY_FOG(i.fogCoord, col);
+                return col;
+            }
+            ENDCG
+        }
+    }
+}
